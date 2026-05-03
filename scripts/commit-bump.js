6592b41e4adcb45d842ed5aaee612d29138c6d37
cnouguier <ch.nouguier@gmail.com>
2026-05-03 22:36:07 +0200

    chore: fix commit-bump script

--- scripts/commit-bump.js
+++ scripts/commit-bump.js
@@ -1,6 +1,9 @@
-const { execSync } = require('child_process')
-const { version } = require('../package.json')
+// scripts/commit-bump.js
+import { execSync } from 'child_process'
+import { readFileSync } from 'fs'
+
+const { version } = JSON.parse(readFileSync('./package.json', 'utf-8'))
 
 execSync('git add .')
-execSync(`git commit -m "chore: bump to v${version}"`)
+execSync(`git commit -m "chore: release v${version}"`)
 execSync('git push')
